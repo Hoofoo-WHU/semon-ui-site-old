32e9db6029db5208f3caa43f1a8d482b746333cd
Hoofoo <wangshuophone@icloud.com>
2019-03-07 09:23:24 +0800

    ⚙ 修改目录配置

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,17 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/WangShuo/Documents/GitHub/semon-ui-site/node_modules/react-static-plugin-typescript",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "/Users/WangShuo/Documents/GitHub/semon-ui-site",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import { Root, Routes } from 'react-static'
-import { Link } from '@reach/router'
-import './app.css'
-import FancyDiv from '@components/FancyDiv'
+import { Root } from 'react-static'
+import Index from './pages/index'
 
-function App() {
-  return (
-    <Root>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-      </nav>
-      <div className="content">
-        <FancyDiv>
-          <Routes />
-        </FancyDiv>
-      </div>
-    </Root>
-  )
-}
-
-export default App
+export default () => (
+  <Root>
+    <Index />
+  </Root>
+)
